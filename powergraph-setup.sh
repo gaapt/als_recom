f80a0dd03829ccedec1bd52959a21cdd5b5e892b
gaapt <gabe@bluefather.com>
2015-04-29 11:37:41 -0300

    Update powergraph-setup.sh

--- powergraph-setup.sh
+++ powergraph-setup.sh
@@ -1,13 +1,3 @@
-#!/usr/bin/env bash
-
-#### Setup graphlab
-
-# Some options that were tried:
-#  - building with tcmalloc leads to seg fault 13 on Mac OS X, and build failure on EC2
-#  - input was split into disjoint parts to maximize GraphLab load speed (not required for Spark or Mahout)
-#  - disabling dynamic graph leads to build failure on EC2
-#  - maxval / minval was set according to
-#   http://bickson.blogspot.com/2013/03/intel-labs-report-on-graphlab-vs-mahout.html
 
 
 # install dependencies across cluster
@@ -28,11 +18,8 @@ git merge brkyvz/mod
 # make on /mnt because compiling requires a bunch of disk space
 # and we kept running out on the 8GB ec2 root volume
 
-#git checkout spark-ec2-build
 
 ./configure --no_tcmalloc
-# we only needed to build the graph-analytics toolkit. Adjust this to build
-# the toolkits you care about. ./configure outputs instructions on this too.
 cd release/toolkits/collaborative_filtering
 # -j8 means use parallel compilation with 8 threads. Adjust to the number of cores you have available.
 make -j8
@@ -41,26 +28,4 @@ cd /mnt
 ~/spark-ec2/copy-dir /mnt/powergraph
 
 cd ~/
-#get pip to install awscli, install aws for easy bucket access
-#wget https://bootstrap.pypa.io/get-pip.py .
-#python get-pip.py
-#sudo pip install awscli
 
-#some export commands to run graphlab more easily not required by this script
-#export HADOOP_HOME="/root/ephemeral-hdfs"
-#export PATH=$HADOOP_HOME:$HADOOP_HOME/bin:$PATH
-#export CLASSPATH=$(hadoop classpath)
-#export GRAPHLAB=/mnt/graphlab/release/toolkits/collaborative_filtering
-
-
-#Sync s3 bucket here
-
-#Run als
-#========
-
-#Before running, don't forget to sync s3 bucket and copy them to hdfs
-
-#mpiexec -n 64 --hostfile /root/spark-ec2/slaves -x CLASSPATH -x GRAPHLAB -x MASTER $GRAPHLAB/als \
-  #--matrix hdfs://$MASTER:9000/amazon-reviewsx8/amatrain.train \
-  #--test hdfs://$MASTER:9000/amazon-reviewsx8/amatest.validate \
-  #--max_iter 10 --D 10 --lambda 0.1 --regnormal 0 --tol 0.0000000001 > testx8-n_64
